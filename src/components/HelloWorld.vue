ce92edccd7ba6f0ae408b47b0a37aadf693f1d78
sheepdog13 <mikecho1234@naver.com>
2023-01-20 14:27:08 +0900

    vueTest

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
+  }
+}
+</script>
+
+<template>
+  <h2>vue로 확인하는 웹페이지 입니다. 아래버튼을 누르면 값이 1씩증가합니다</h2>
+  <button v-on:click="increment">{{ count }}+</button>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/components/HomeVue.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
+  }
+}
+</script>
+
+<template>
+  <h2>vue로 확인하는 웹페이지 입니다. 아래버튼을 누르면 값이 1씩증가합니다</h2>
+  <button v-on:click="increment">{{ count }}+</button>
+</template>
