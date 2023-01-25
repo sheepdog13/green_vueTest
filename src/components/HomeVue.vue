c0504ed93461409e718b7b72c81c433ed16e5b5f
sheepdog13 <mikecho1234@naver.com>
2023-01-25 14:41:10 +0900

    0125 수정

--- src/components/HomeVue.vue
+++ src/components/HomeVue.vue
@@ -2,7 +2,8 @@
 export default {
   data() {
     return {
-      count: 0
+      count: 0,
+      titleClass : "title"
     }
   },
   methods: {
@@ -14,6 +15,11 @@ export default {
 </script>
 
 <template>
+  <h1 :class="titleClass">{{count}}</h1>
   <h2>vue로 확인하는 웹페이지 입니다. 아래버튼을 누르면 값이 1씩증가합니다</h2>
-  <button v-on:click="increment">{{ count }}+</button>
+  <button v-on:click="increment">1+</button>
 </template>
+
+<style>
+  .title {color: red;}
+</style>
